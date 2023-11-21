00c1a92be10b5311cd027850797dfd0f20d19e91
Matheus Cabral <matheuscabraldesousa@gmail.com>
2023-11-21 20:07:37 -0300

    creation of false data

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id": 1,
+    "photo": "aaa",
+    "title": "qualque coisa",
+    "description": "ola"
+  },
+  {
+    "id": 1,
+    "photo": "aaa",
+    "title": "qualque coisa",
+    "description": "ola"
+  }
+]
