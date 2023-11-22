c1f4ef67246892e0d3c80b6a7a58f2b296e82687
Matheus Cabral <matheuscabraldesousa@gmail.com>
2023-11-22 18:02:50 -0300

    adjusting false data

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,14 @@
 export const dataFake = [
   {
-    "id": 1,
-    "photo": "aaa",
+    "id": "1",
     "title": "qualque coisa",
-    "description": "ola"
+    "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis.",
+    "photoCover": "https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
   },
   {
-    "id": 1,
-    "photo": "aaa",
-    "title": "qualque coisa",
-    "description": "ola"
+    "id": "2",
+    "title": "qualque coisa 2 ",
+    "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis.",
+    "photoCover": "https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
   }
 ]
