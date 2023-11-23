8746257249378fecb556056a329d5a2ef0de1275
Matheus Cabral <matheuscabraldesousa@gmail.com>
2023-11-23 20:41:33 -0300

    data change

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,14 +1,26 @@
 export const dataFake = [
   {
     "id": "1",
-    "title": "qualque coisa",
-    "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis.",
-    "photoCover": "https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+    "title": "Lasanha à Bolonhesa",
+    "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis.",
+    "photoCover": "https://i.ytimg.com/vi/FrdumyhYaZY/maxresdefault.jpg"
   },
   {
     "id": "2",
-    "title": "qualque coisa 2 ",
-    "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis.",
-    "photoCover": "https://www.unfe.org/wp-content/uploads/2019/04/SM-placeholder.png"
+    "title": "Mousse de maracujá",
+    "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis.",
+    "photoCover": "https://anamariabraga.globo.com/wp-content/uploads/2019/06/mousse-de-maracuja-facil-e-pratica.jpg"
+  },
+  {
+    "id": "3",
+    "title": "Bolo de Chocolate",
+    "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis.",
+    "photoCover": "https://www.receitasnestle.com.br/sites/default/files/srh_recipes/b6a911547d2a4fa622501fbea4a8e3ce.jpg"
+  },
+  {
+    "id": "4",
+    "title": "Churros Caseiros",
+    "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis.Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus odio libero molestias, atque fugiat reprehenderit laboriosam soluta debitis illum alias reiciendis, dicta illo. Minima doloremque mollitia accusamus quo. Quod, blanditiis.",
+    "photoCover": "https://st1.uvnimg.com/dims4/default/117adf0/2147483647/thumbnail/400x225/quality/75/?url=http%3A%2F%2Fuvn-brightspot.s3.amazonaws.com%2F99%2Feb%2F6b864532432c8f865bc16439c09e%2Fchurros-com-chocolate-0722-1400x800.jpg"
   }
 ]
